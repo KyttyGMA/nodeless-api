6e354a4587d488cbd242d81969cf201e951cebb0
KyttyGMA <everunner673@gmail.com>
2024-09-10 08:12:51 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,17 @@ const GOLD_API_URL = 'https://api.gold-api.com/price/XAU';
 
 module.exports = async (req, res) => {
   try {
+    // Configura los encabezados CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Maneja las solicitudes OPTIONS para CORS preflight
+    if (req.method === 'OPTIONS') {
+      res.status(204).end();
+      return;
+    }
+
     const data = await new Promise((resolve, reject) => {
       http.get(GOLD_API_URL, (response) => {
         let body = '';
@@ -31,7 +42,7 @@ module.exports = async (req, res) => {
 
     // Calcula cuántas mandarinas se pueden comprar
     const mandarinsCanBuy = goldPriceUSD / MANDARIN_PRICE_USD;
-    res.status(200).send(mandarinsCanBuy);
+    res.status(200).send(mandarinsCanBuy.toString()); // Asegúrate de enviar como cadena
   } catch (error) {
     // Maneja errores
     res.status(500).json({ error: 'Error fetching gold price' });
